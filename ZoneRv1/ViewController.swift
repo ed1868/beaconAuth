31463942492c36731f6fc2cd4897639a9f7b8c93
ed1868 <ruizeduardo21@gmail.com>
2020-09-01 23:45:18 -0400

    first beacon auth comit

---
+++ ZoneRv1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ZoneRv1
+//
+//  Created by Eddie Ruiz on 9/1/20.
+//  Copyright © 2020 AI Nomads. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ZoneRv1Tests/ZoneRv1Tests.swift
@@ -0,0 +1,34 @@
+//
+//  ZoneRv1Tests.swift
+//  ZoneRv1Tests
+//
+//  Created by Eddie Ruiz on 9/1/20.
+//  Copyright © 2020 AI Nomads. All rights reserved.
+//
+
+import XCTest
+@testable import ZoneRv1
+
+class ZoneRv1Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
